2f4adcd925884e4e5aa99a85dbab47d2b4ab5d31
emanuel <schezemanuel@gmail.com>
2022-07-16 01:05:14 -0300

    Conexión a mysql y test

--- src/app.py
+++ src/app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 mysql = MySQL()
 
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'products'
+
+mysql.init_app(app)
+
+
+@app.route('/')
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql = "insert into tech(image,name,description,stock) values ('vr.img','virtual headset','is like a helm that show  virtual reality','100');"
+    cursor.execute(sql)
+
+    conn.commit()
+
+    return render_template('products/index.html')
+
+
 if __name__ == '__main__':
-    app.run(debuh=True)
+    app.run(debug=True)
