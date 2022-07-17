22f5436fcd74e0710e61a7a6745c734f05098b60
emanuel <schezemanuel@gmail.com>
2022-07-17 20:29:38 -0300

    Creamos rutas para cargar productos

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -19,12 +19,37 @@ def index():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    sql = "insert into tech(image,name,description,stock) values ('vr.img','virtual headset','is like a helm that show  virtual reality','100');"
+    sql = "SELECT * FROM tech;"
     cursor.execute(sql)
+
+    tech = cursor.fetchall()
 
     conn.commit()
 
-    return render_template('products/index.html')
+    return render_template('products/index.html', tech=tech)
+
+
+@app.route('/create')
+def create():
+    return render_template('products/create.html')
+
+
+@app.route('/store', methods=["POST"])
+def store():
+    _nombre = request.form["txtNombre"]
+    _description = request.form["txtDescription"]
+    _stock = request.form["txtStock"]
+    _foto = request.files["txtFoto"]
+
+    sql = "INSERT INTO tech (name, description, stock, image) values (%s, %s, %s, %s);"
+    datos = (_nombre, _description, _stock, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
